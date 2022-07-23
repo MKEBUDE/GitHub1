21c132b8cdb6b598ef5be5a21b8a1644f90f7a47
MKEBUDE <kebudemurat@hotmail.com>
2022-07-23 19:57:26 -0400

    paketleri yeniden verdik

--- GithubExample/src/GithHUBEX/CLASS1.java
+++ GithubExample/src/GithHUBEX/CLASS1.java
@@ -3,14 +3,14 @@ package GithHUBEX;
 public class CLASS1 {
     public static void main(String[] args) {
 
-int i=10;
+int i=10git ;
         
- for(i){
-system.out.println("hello world);
-                   
-                   }
-        
-        
-        
+ while(i<=25){
+
+
+
+System.out.println(i);
+     i+=3;
+ }
     }
 }
