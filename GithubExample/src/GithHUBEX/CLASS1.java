ef63b0f924b36d25d9e8369c0829090676656195
MKEBUDE <108554028+MKEBUDE@users.noreply.github.com>
2022-07-23 19:33:27 -0400

    Update CLASS1.java
    we have done some new changes inclueding new codes

--- GithubExample/src/GithHUBEX/CLASS1.java
+++ GithubExample/src/GithHUBEX/CLASS1.java
@@ -3,6 +3,14 @@ package GithHUBEX;
 public class CLASS1 {
     public static void main(String[] args) {
 
-
+int i=10;
+        
+ for(i){
+system.out.println("hello world);
+                   
+                   }
+        
+        
+        
     }
 }
