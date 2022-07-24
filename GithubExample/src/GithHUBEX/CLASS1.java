166ee99cfe9c4915a06f47e33ae0f9460d3b45c5
MKEBUDE <108554028+MKEBUDE@users.noreply.github.com>
2022-07-23 20:03:42 -0400

    Update CLASS1.java

--- GithubExample/src/GithHUBEX/CLASS1.java
+++ GithubExample/src/GithHUBEX/CLASS1.java
@@ -4,11 +4,8 @@ public class CLASS1 {
     public static void main(String[] args) {
 
 int i=10 ;
-        
+     //while loop is similar to if statement   
  while(i<=25){
-
-
-
 System.out.println(i);
      i+=3;
  }
